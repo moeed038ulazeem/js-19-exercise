d3452cbcd61bbd5c65df35ec42b6efd1b48f7ba9
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-08-24 06:19:36 -0700

    assignment-19

---
+++ java.js
@@ -0,0 +1,41 @@
+function trafficLight(x){
+    switch(x)
+    {
+        case "Red":
+            console.log("Stop")
+            break;
+        case "Orange":
+            console.log("tiyar")
+            break;
+        case "Green":
+        console.log("Go")
+        break ;
+        default :
+        console.log("Dafa hu jahu")
+    }
+}
+trafficLight("Ayesha")
+
+function fruitName(x){
+    switch(x)
+    {
+        case "Apple":
+        console.log("1kg: 200/Rs")
+        break;
+
+        case "Peace":
+        console.log("1kg: 100/Rs")
+        break;
+
+        case "Mango":
+        console.log("1kg: 250/Rs")
+        break;
+
+        case "Banana":
+        console.log("1kg: 80/Rs")
+        break;
+        default :
+        console.log("invalid friut name")
+    }
+}
+fruitName("Apple")
